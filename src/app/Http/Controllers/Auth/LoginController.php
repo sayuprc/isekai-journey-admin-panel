f2cb0944d75938c2c31a5b0938eb70f18411a6c6
sayuprc <bepdielibe+github@gmail.com>
2024-08-25 00:01:50 +0900

    リダイレクト先変更

--- src/app/Http/Controllers/Auth/LoginController.php
+++ src/app/Http/Controllers/Auth/LoginController.php
@@ -27,7 +27,7 @@ class LoginController extends Controller
         if ($this->authManager->guard()->attempt($request->validated())) {
             $request->session()->regenerate();
 
-            return redirect()->route('home');
+            return redirect()->route('journey-logs.index');
         }
 
         return back()->withErrors([
