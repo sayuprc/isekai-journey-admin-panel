d5fd7ae99a572c9e0f09907bcb3fd04c02d40993
sayuprc <bepdielibe+github@gmail.com>
2024-11-17 23:39:04 +0900

    ドメインモデルを作成

---
+++ src/app/Features/Song/Domain/Entities/ReleasedOn.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Features\Song\Domain\Entities;
+
+use App\Shared\Domain\DateTimeValueObject;
+
+class ReleasedOn extends DateTimeValueObject
+{
+}
