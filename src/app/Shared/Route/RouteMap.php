708af2aedc6fda73225889dbc5aa9715e462f4f0
sayuprc <bepdielibe+github@gmail.com>
2024-11-18 00:10:09 +0900

    ルーティング周りを実装

--- src/app/Shared/Route/RouteMap.php
+++ src/app/Shared/Route/RouteMap.php
@@ -6,18 +6,26 @@ namespace App\Shared\Route;
 
 enum RouteMap: string
 {
+    // システム関連
     case SHOW_LOGIN_FORM = 'login';
     case LOGIN = 'login.handle';
+
+    // 軌跡関連
     case LIST_JOURNEY_LOGS = 'journey-logs.index';
     case SHOW_CREATE_JOURNEY_LOG_FORM = 'journey-logs.create.index';
     case CREATE_JOURNEY_LOG = 'journey-logs.create.handle';
     case SHOW_EDIT_JOURNEY_LOG_FORM = 'journey-logs.edit.index';
     case EDIT_JOURNEY_LOG = 'journey-logs.edit.handle';
     case DELETE_JOURNEY_LOG = 'journey-logs.delete.handle';
+
+    // 軌跡リンク種別関連
     case LIST_JOURNEY_LOG_LINK_TYPE = 'journey-log-link-types.index';
     case SHOW_CREATE_JOURNEY_LOG_LINK_TYPE_FORM = 'journey-log-link-types.create.index';
     case CREATE_JOURNEY_LOG_LINK_TYPE = 'journey-log-link-types.create.handle';
     case SHOW_EDIT_JOURNEY_LOG_LINK_TYPE_FORM = 'journey-log-link-types.edit.index';
     case EDIT_JOURNEY_LOG_LINK_TYPE = 'journey-log-link-types.edit.handle';
     case DELETE_JOURNEY_LOG_LINK_TYPE = 'journey-log-link-types.delete.handle';
+
+    // 楽曲関連
+    case LIST_SONGS = 'songs.index';
 }
