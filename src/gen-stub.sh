48653ceeb6ed8358e1f3dddf5ea9b044cdfc51fb
sayuprc <bepdielibe+github@gmail.com>
2024-11-17 23:38:23 +0900

    スタブファイル生成コマンドを修正

--- src/gen-stub.sh
+++ src/gen-stub.sh
@@ -6,6 +6,7 @@ protoc --proto_path=proto \
   --plugin=protoc-gen-grpc=/usr/local/bin/grpc_php_plugin \
   ./proto/shared/shared.proto \
   ./proto/journey_log/journey_log.proto \
-  ./proto/journey_log_link_type/journey_log_link_type.proto
+  ./proto/journey_log_link_type/journey_log_link_type.proto \
+  ./proto/song/song.proto
 
 composer dump-autoload
