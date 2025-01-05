19c1872a0d59e6afa8c0158cb5d72666a6481a0d
sayuprc <bepdielibe+github@gmail.com>
2025-01-05 20:49:20 +0900

    ServiceProvider を分割

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -4,65 +4,23 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
-use App\Features\JourneyLog\Domain\Repositories\JourneyLogRepositoryInterface;
-use App\Features\JourneyLog\Infrastructures\Repositories\JourneyLogRepository;
-use App\Features\JourneyLogLinkType\Domain\Repositories\JourneyLogLinkTypeRepositoryInterface;
-use App\Features\JourneyLogLinkType\Infrastructures\Repositories\JourneyLogLinkTypeRepository;
-use App\Features\Song\Domain\Repositories\SongRepositoryInterface;
-use App\Features\Song\Infrastructures\Repositories\SongRepository;
 use App\Shared\Application\Uuid\DummyUuidGenerator;
 use App\Shared\Uuid\UuidGeneratorInterface;
-use Generated\IsekaiJourney\JourneyLog\JourneyLogServiceClient;
-use Generated\IsekaiJourney\JourneyLogLinkType\JourneyLogLinkTypeServiceClient;
-use Generated\IsekaiJourney\Song\SongServiceClient;
-use Grpc\ChannelCredentials;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
-        $config = fn (): array => [
-            config('grpc.api_url'),
-            ChannelCredentials::createSsl(file_get_contents(config('grpc.root_ca'))),
-        ];
-
-        $this->app->bind(JourneyLogServiceClient::class, function () use ($config): JourneyLogServiceClient {
-            [$url, $credentials] = $config();
-
-            return new JourneyLogServiceClient($url, ['credentials' => $credentials]);
-        });
-
-        $this->app->bind(JourneyLogLinkTypeServiceClient::class, function () use ($config): JourneyLogLinkTypeServiceClient {
-            [$url, $credentials] = $config();
-
-            return new JourneyLogLinkTypeServiceClient($url, ['credentials' => $credentials]);
-        });
-
-        $this->app->bind(SongServiceClient::class, function () use ($config): SongServiceClient {
-            [$url, $credentials] = $config();
-
-            return new SongServiceClient($url, ['credentials' => $credentials]);
-        });
-
-        $this->app->bind(JourneyLogRepositoryInterface::class, JourneyLogRepository::class);
-
-        $this->app->bind(JourneyLogLinkTypeRepositoryInterface::class, JourneyLogLinkTypeRepository::class);
-
-        $this->app->bind(SongRepositoryInterface::class, SongRepository::class);
-
         $this->app->bind(UuidGeneratorInterface::class, DummyUuidGenerator::class);
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        URL::forceScheme(str_starts_with(config('app.url'), 'https') ? 'https' : 'http');
+        $url = config('app.url');
+        assert(is_string($url));
+
+        URL::forceScheme(str_starts_with($url, 'https') ? 'https' : 'http');
     }
 }
--- src/bootstrap/providers.php
+++ src/bootstrap/providers.php
@@ -4,4 +4,6 @@ declare(strict_types=1);
 
 return [
     App\Providers\AppServiceProvider::class,
+    App\Providers\GrpcServiceProvider::class,
+    App\Providers\ProdServiceProvider::class,
 ];
