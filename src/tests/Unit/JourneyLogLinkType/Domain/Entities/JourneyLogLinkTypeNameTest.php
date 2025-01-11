c576327ec206be6e9255932bb613553ec7d39f02
sayuprc <bepdielibe+github@gmail.com>
2025-01-11 21:19:46 +0900

    既存テストを改修

--- src/tests/Unit/JourneyLogLinkType/Domain/Entities/JourneyLogLinkTypeNameTest.php
+++ src/tests/Unit/JourneyLogLinkType/Domain/Entities/JourneyLogLinkTypeNameTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Unit\JourneyLogLinkType\Domain\Entities;
 
 use JourneyLogLinkType\Domain\Entities\JourneyLogLinkTypeName;
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use Shared\Domain\Exceptions\InvalidDomainException;
 use Tests\TestCase;
@@ -12,21 +13,37 @@ use Tests\TestCase;
 class JourneyLogLinkTypeNameTest extends TestCase
 {
     #[Test]
-    public function successful(): void
+    #[DataProvider('validData')]
+    public function canBeInstanced(string $value): void
     {
-        $value = 'a';
-
         $story = new JourneyLogLinkTypeName($value);
 
         $this->assertSame($value, $story->value);
     }
 
+    public static function validData(): array
+    {
+        return [
+            ['a'],
+            ['あ'],
+            ['a b c あ'],
+        ];
+    }
+
     #[Test]
-    public function emptyValue(): void
+    #[DataProvider('invalidData')]
+    public function invalidValue(string $value): void
     {
         $this->expectException(InvalidDomainException::class);
         $this->expectExceptionMessage('journey log link type name must be at least 1 characters');
 
-        new JourneyLogLinkTypeName('');
+        new JourneyLogLinkTypeName($value);
+    }
+
+    public static function invalidData(): array
+    {
+        return [
+            [''],
+        ];
     }
 }
--- src/tests/Unit/Shared/Domain/UuidValueObjectTest.php
+++ src/tests/Unit/Shared/Domain/UuidValueObjectTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tests\Unit\Shared\Domain;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use Shared\Domain\Exceptions\InvalidDomainException;
 use Shared\Domain\UuidValueObject;
@@ -22,21 +23,21 @@ class UuidValueObjectTest extends TestCase
     }
 
     #[Test]
-    public function emptyValue(): void
+    #[DataProvider('invalidData')]
+    public function formatIsInvalid(string $value): void
     {
         $this->expectException(InvalidDomainException::class);
         $this->expectExceptionMessage('Value format is invalid');
 
-        new Uuid('');
+        new Uuid($value);
     }
 
-    #[Test]
-    public function invalidFormat(): void
+    public static function invalidData(): array
     {
-        $this->expectException(InvalidDomainException::class);
-        $this->expectExceptionMessage('Value format is invalid');
-
-        new Uuid('invalid format');
+        return [
+            [''],
+            ['invalid format'],
+        ];
     }
 }
 
