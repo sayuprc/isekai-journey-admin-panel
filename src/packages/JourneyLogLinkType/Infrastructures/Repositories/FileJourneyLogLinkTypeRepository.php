1b7d10db89f7c8dc6bb711d270830a1e825c8a85
sayuprc <bepdielibe+github@gmail.com>
2025-03-23 19:52:21 +0900

    ファイルリポジトリを作成

---
+++ src/packages/JourneyLogLinkType/Infrastructures/Repositories/FileJourneyLogLinkTypeRepository.php
@@ -0,0 +1,53 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JourneyLogLinkType\Infrastructures\Repositories;
+
+use JourneyLogLinkType\Domain\Entities\JourneyLogLinkType;
+use JourneyLogLinkType\Domain\Entities\JourneyLogLinkTypeId;
+use JourneyLogLinkType\Domain\Repositories\JourneyLogLinkTypeRepositoryInterface;
+use Shared\Repository\FileStore;
+
+class FileJourneyLogLinkTypeRepository implements JourneyLogLinkTypeRepositoryInterface
+{
+    private const string PATH = 'journey-log-link-types.dat';
+
+    /**
+     * @param FileStore<JourneyLogLinkType> $store
+     */
+    public function __construct(private readonly FileStore $store)
+    {
+    }
+
+    /**
+     * @return array<JourneyLogLinkType>
+     */
+    public function listJourneyLogLinkTypes(): array
+    {
+        return array_values($this->store->getAll(self::PATH));
+    }
+
+    public function createJourneyLogLinkType(JourneyLogLinkType $journeyLogLinkType): void
+    {
+        $this->store->put(self::PATH, $journeyLogLinkType->journeyLogLinkTypeId->value, $journeyLogLinkType);
+    }
+
+    public function getJourneyLogLinkType(JourneyLogLinkTypeId $journeyLogLinkTypeId): JourneyLogLinkType
+    {
+        $found = $this->store->get(self::PATH, $journeyLogLinkTypeId->value);
+        assert($found instanceof JourneyLogLinkType);
+
+        return $found;
+    }
+
+    public function editJourneyLogLinkType(JourneyLogLinkType $journeyLogLinkType): void
+    {
+        $this->store->put(self::PATH, $journeyLogLinkType->journeyLogLinkTypeId->value, $journeyLogLinkType);
+    }
+
+    public function deleteJourneyLogLinkType(JourneyLogLinkTypeId $journeyLogLinkTypeId): void
+    {
+        $this->store->unset(self::PATH, $journeyLogLinkTypeId->value);
+    }
+}
