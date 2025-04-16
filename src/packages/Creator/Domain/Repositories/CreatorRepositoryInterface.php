f3277741cb582d6d2f7e9245666555d7ecc35636
sayuprc <bepdielibe+github@gmail.com>
2025-04-16 22:30:33 +0900

    リポジトリの実装を追加

--- src/packages/Creator/Domain/Repositories/CreatorRepositoryInterface.php
+++ src/packages/Creator/Domain/Repositories/CreatorRepositoryInterface.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Creator\Domain\Repositories;
 
 use Creator\Domain\Models\Creator;
+use Creator\Domain\Models\CreatorId;
 
 interface CreatorRepositoryInterface
 {
@@ -14,4 +15,6 @@ interface CreatorRepositoryInterface
     public function listCreators(): array;
 
     public function createCreator(Creator $creator): void;
+
+    public function getCreator(CreatorId $creatorId): Creator;
 }
