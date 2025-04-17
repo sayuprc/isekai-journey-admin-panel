178f33aa152626edf01266b7dc237f5439490883
sayuprc <bepdielibe+github@gmail.com>
2025-04-17 22:15:26 +0900

    ユースケースの実装を追加

---
+++ src/packages/Creator/Application/Edit/EditInteractor.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Creator\Application\Edit;
+
+use Creator\Domain\Models\Creator;
+use Creator\Domain\Models\CreatorId;
+use Creator\Domain\Models\CreatorName;
+use Creator\Domain\Repositories\CreatorRepositoryInterface;
+use Creator\UseCases\Edit\EditRequest;
+use Creator\UseCases\Edit\EditUseCaseInterface;
+
+class EditInteractor implements EditUseCaseInterface
+{
+    public function __construct(private readonly CreatorRepositoryInterface $client)
+    {
+    }
+
+    public function handle(EditRequest $request): void
+    {
+        $creator = new Creator(
+            new CreatorId($request->creatorId),
+            new CreatorName($request->creatorName),
+        );
+
+        $this->client->editCreator($creator);
+    }
+}
