7151fd5b0c74f8dd8876b1ea0123db00b6ce848e
sayuprc <bepdielibe+github@gmail.com>
2025-04-19 09:36:09 +0900

    UseCase を修正

--- src/packages/Creator/UseCases/Get/GetUseCaseInterface.php
+++ src/packages/Creator/UseCases/Get/GetUseCaseInterface.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Creator\UseCases\Get;
 
+use Support\Result\Result;
+
 interface GetUseCaseInterface
 {
-    public function handle(GetRequest $request): GetResponse;
+    /**
+     * @return Result<GetResponse, string>
+     */
+    public function handle(GetRequest $request): Result;
 }
