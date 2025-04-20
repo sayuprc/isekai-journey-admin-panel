14ff2379389461390ba40db5bfcdc9b8ee639591
sayuprc <bepdielibe+github@gmail.com>
2025-04-20 21:14:05 +0900

    JourneyLogLinkTypeFactoryInterface とその実装を追加

---
+++ src/tests/Unit/JourneyLogLinkType/Infrastructures/Factories/JourneyLogLinkTypeFactoryTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\JourneyLogLinkType\Infrastructures\Factories;
+
+use JourneyLogLinkType\Infrastructures\Factories\JourneyLogLinkTypeFactory;
+use Mockery;
+use Mockery\MockInterface;
+use PHPUnit\Framework\Attributes\Test;
+use Support\Uuid\UuidGeneratorInterface;
+use Tests\TestCase;
+
+class JourneyLogLinkTypeFactoryTest extends TestCase
+{
+    private MockInterface&UuidGeneratorInterface $uuid;
+
+    private JourneyLogLinkTypeFactory $factory;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->uuid = Mockery::mock(UuidGeneratorInterface::class);
+
+        $this->factory = new JourneyLogLinkTypeFactory($this->uuid);
+    }
+
+    #[Test]
+    public function create(): void
+    {
+        $this->uuid->shouldReceive('generate')
+            ->andReturn('AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA')
+            ->once();
+
+        $journeyLogLinkType = $this->factory->create(
+            'リンク',
+            1,
+        );
+
+        $this->assertSame('AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA', $journeyLogLinkType->journeyLogLinkTypeId->value);
+        $this->assertSame('リンク', $journeyLogLinkType->journeyLogLinkTypeName->value);
+        $this->assertSame(1, $journeyLogLinkType->orderNo->value);
+    }
+
+    #[Test]
+    public function reconstitute(): void
+    {
+        $journeyLogLinkType = $this->factory->reconstitute(
+            'AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA',
+            'リンク',
+            1,
+        );
+
+        $this->assertSame('AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA', $journeyLogLinkType->journeyLogLinkTypeId->value);
+        $this->assertSame('リンク', $journeyLogLinkType->journeyLogLinkTypeName->value);
+        $this->assertSame(1, $journeyLogLinkType->orderNo->value);
+    }
+}
