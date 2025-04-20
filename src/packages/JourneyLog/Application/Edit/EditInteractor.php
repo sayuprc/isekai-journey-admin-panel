077ae421e5843a4c8dc5c760d02e5f735d3ed527
sayuprc <bepdielibe+github@gmail.com>
2025-04-20 21:00:14 +0900

    UseCase で JourneyLogFactoryInterface を利用するように修正

--- src/packages/JourneyLog/Application/Edit/EditInteractor.php
+++ src/packages/JourneyLog/Application/Edit/EditInteractor.php
@@ -4,68 +4,30 @@ declare(strict_types=1);
 
 namespace JourneyLog\Application\Edit;
 
-use DateTimeImmutable;
-use JourneyLog\Domain\Models\FromOn;
-use JourneyLog\Domain\Models\JourneyLog;
-use JourneyLog\Domain\Models\JourneyLogId;
-use JourneyLog\Domain\Models\JourneyLogLink\JourneyLogLink;
-use JourneyLog\Domain\Models\JourneyLogLink\JourneyLogLinkId;
-use JourneyLog\Domain\Models\JourneyLogLink\JourneyLogLinkName;
-use JourneyLog\Domain\Models\Period;
-use JourneyLog\Domain\Models\Story;
-use JourneyLog\Domain\Models\ToOn;
-use JourneyLog\Domain\Models\Url;
+use JourneyLog\Domain\Models\JourneyLogFactoryInterface;
 use JourneyLog\Domain\Repositories\JourneyLogRepositoryInterface;
-use JourneyLog\UseCases\Edit\EditJourneyLogLink;
 use JourneyLog\UseCases\Edit\EditRequest;
 use JourneyLog\UseCases\Edit\EditUseCaseInterface;
-use JourneyLogLinkType\Domain\Models\JourneyLogLinkTypeId;
-use Support\Domain\ValueObjects\OrderNo;
-use Support\Uuid\UuidGeneratorInterface;
 
 class EditInteractor implements EditUseCaseInterface
 {
     public function __construct(
         private readonly JourneyLogRepositoryInterface $repository,
-        private readonly UuidGeneratorInterface $uuid,
+        private readonly JourneyLogFactoryInterface $factory,
     ) {
     }
 
     public function handle(EditRequest $request): void
     {
-        $journeyLog = new JourneyLog(
-            new JourneyLogId($request->journeyLogId),
-            new Story($request->story),
-            new Period(
-                new FromOn(new DateTimeImmutable($request->fromOn)),
-                new ToOn(new DateTimeImmutable($request->toOn))
-            ),
-            new OrderNo($request->orderNo),
-            $this->toJourneyLogLinks($request->journeyLogLinks),
+        $journeyLog = $this->factory->createForUpdate(
+            $request->journeyLogId,
+            $request->story,
+            $request->fromOn,
+            $request->toOn,
+            $request->orderNo,
+            $request->journeyLogLinks,
         );
 
         $this->repository->update($journeyLog);
-    }
-
-    /**
-     * @param array<EditJourneyLogLink> $data
-     *
-     * @return array<JourneyLogLink>
-     */
-    private function toJourneyLogLinks(array $data): array
-    {
-        $journeyLogLinks = [];
-
-        foreach ($data as $link) {
-            $journeyLogLinks[] = new JourneyLogLink(
-                new JourneyLogLinkId($this->uuid->generate()), // リンクはデリートインサートなのでダミー値でよい
-                new JourneyLogLinkName($link->journeyLogLinkName),
-                new Url($link->url),
-                new OrderNo($link->orderNo),
-                new JourneyLogLinkTypeId($link->journeyLogLinkTypeId),
-            );
-        }
-
-        return $journeyLogLinks;
     }
 }
