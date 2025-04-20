0f45ffacb7d947068a38c5d8800a3dfa0d6f6184
sayuprc <bepdielibe+github@gmail.com>
2025-04-20 21:47:12 +0900

    UseCase で CreatorFactoryInterface を利用するように修正

--- src/packages/Creator/Application/Edit/EditInteractor.php
+++ src/packages/Creator/Application/Edit/EditInteractor.php
@@ -4,25 +4,22 @@ declare(strict_types=1);
 
 namespace Creator\Application\Edit;
 
-use Creator\Domain\Models\Creator;
-use Creator\Domain\Models\CreatorId;
-use Creator\Domain\Models\CreatorName;
+use Creator\Domain\Models\CreatorFactoryInterface;
 use Creator\Domain\Repositories\CreatorRepositoryInterface;
 use Creator\UseCases\Edit\EditRequest;
 use Creator\UseCases\Edit\EditUseCaseInterface;
 
 class EditInteractor implements EditUseCaseInterface
 {
-    public function __construct(private readonly CreatorRepositoryInterface $repository)
-    {
+    public function __construct(
+        private readonly CreatorRepositoryInterface $repository,
+        private readonly CreatorFactoryInterface $factory,
+    ) {
     }
 
     public function handle(EditRequest $request): void
     {
-        $creator = new Creator(
-            new CreatorId($request->creatorId),
-            new CreatorName($request->creatorName),
-        );
+        $creator = $this->factory->reconstitute($request->creatorId, $request->creatorName);
 
         $this->repository->update($creator);
     }
