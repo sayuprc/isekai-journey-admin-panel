05ad67ed2ac304ed397b47c7efbca29d9ccb23fc
sayuprc <bepdielibe+github@gmail.com>
2025-04-20 21:00:14 +0900

    UseCase で JourneyLogFactoryInterface を利用するように修正

--- src/packages/JourneyLog/UseCases/Edit/EditRequest.php
+++ src/packages/JourneyLog/UseCases/Edit/EditRequest.php
@@ -4,17 +4,20 @@ declare(strict_types=1);
 
 namespace JourneyLog\UseCases\Edit;
 
+use DateTimeInterface;
+use JourneyLog\Domain\Dtos\CreateJourneyLogLinkData;
+
 class EditRequest
 {
     /**
-     * @param positive-int              $orderNo
-     * @param array<EditJourneyLogLink> $journeyLogLinks
+     * @param positive-int                    $orderNo
+     * @param array<CreateJourneyLogLinkData> $journeyLogLinks
      */
     public function __construct(
         public readonly string $journeyLogId,
         public readonly string $story,
-        public readonly string $fromOn,
-        public readonly string $toOn,
+        public readonly DateTimeInterface $fromOn,
+        public readonly DateTimeInterface $toOn,
         public readonly int $orderNo,
         public readonly array $journeyLogLinks,
     ) {
