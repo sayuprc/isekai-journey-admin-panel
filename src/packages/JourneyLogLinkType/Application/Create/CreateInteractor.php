6644dddb2b6c423e755c8e950fde355df0ea666f
sayuprc <bepdielibe+github@gmail.com>
2025-04-20 21:23:56 +0900

    UseCase で JourneyLogLinkTypeFactoryInterface を利用するように修正

--- src/packages/JourneyLogLinkType/Application/Create/CreateInteractor.php
+++ src/packages/JourneyLogLinkType/Application/Create/CreateInteractor.php
@@ -4,30 +4,22 @@ declare(strict_types=1);
 
 namespace JourneyLogLinkType\Application\Create;
 
-use JourneyLogLinkType\Domain\Models\JourneyLogLinkType;
-use JourneyLogLinkType\Domain\Models\JourneyLogLinkTypeId;
-use JourneyLogLinkType\Domain\Models\JourneyLogLinkTypeName;
+use JourneyLogLinkType\Domain\Models\JourneyLogLinkTypeFactoryInterface;
 use JourneyLogLinkType\Domain\Repositories\JourneyLogLinkTypeRepositoryInterface;
 use JourneyLogLinkType\UseCases\Create\CreateRequest;
 use JourneyLogLinkType\UseCases\Create\CreateUseCaseInterface;
-use Support\Domain\ValueObjects\OrderNo;
-use Support\Uuid\UuidGeneratorInterface;
 
 class CreateInteractor implements CreateUseCaseInterface
 {
     public function __construct(
         private readonly JourneyLogLinkTypeRepositoryInterface $repository,
-        private readonly UuidGeneratorInterface $uuid,
+        private readonly JourneyLogLinkTypeFactoryInterface $factory,
     ) {
     }
 
     public function handle(CreateRequest $request): void
     {
-        $journeyLogLinkType = new JourneyLogLinkType(
-            new JourneyLogLinkTypeId($this->uuid->generate()),
-            new JourneyLogLinkTypeName($request->journeyLogLinkTypeName),
-            new OrderNo($request->orderNo),
-        );
+        $journeyLogLinkType = $this->factory->create($request->journeyLogLinkTypeName, $request->orderNo);
 
         $this->repository->insert($journeyLogLinkType);
     }
--- src/packages/JourneyLogLinkType/Application/Edit/EditInteractor.php
+++ src/packages/JourneyLogLinkType/Application/Edit/EditInteractor.php
@@ -4,26 +4,25 @@ declare(strict_types=1);
 
 namespace JourneyLogLinkType\Application\Edit;
 
-use JourneyLogLinkType\Domain\Models\JourneyLogLinkType;
-use JourneyLogLinkType\Domain\Models\JourneyLogLinkTypeId;
-use JourneyLogLinkType\Domain\Models\JourneyLogLinkTypeName;
+use JourneyLogLinkType\Domain\Models\JourneyLogLinkTypeFactoryInterface;
 use JourneyLogLinkType\Domain\Repositories\JourneyLogLinkTypeRepositoryInterface;
 use JourneyLogLinkType\UseCases\Edit\EditRequest;
 use JourneyLogLinkType\UseCases\Edit\EditUseCaseInterface;
-use Support\Domain\ValueObjects\OrderNo;
 
 class EditInteractor implements EditUseCaseInterface
 {
-    public function __construct(private readonly JourneyLogLinkTypeRepositoryInterface $repository)
-    {
+    public function __construct(
+        private readonly JourneyLogLinkTypeRepositoryInterface $repository,
+        private readonly JourneyLogLinkTypeFactoryInterface $factory,
+    ) {
     }
 
     public function handle(EditRequest $request): void
     {
-        $journeyLogLinkType = new JourneyLogLinkType(
-            new JourneyLogLinkTypeId($request->journeyLogLinkTypeId),
-            new JourneyLogLinkTypeName($request->journeyLogLinkTypeName),
-            new OrderNo($request->orderNo),
+        $journeyLogLinkType = $this->factory->reconstitute(
+            $request->journeyLogLinkTypeId,
+            $request->journeyLogLinkTypeName,
+            $request->orderNo,
         );
 
         $this->repository->update($journeyLogLinkType);
