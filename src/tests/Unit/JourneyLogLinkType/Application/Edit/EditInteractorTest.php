1785e73d6368c87e6fbc90a9c2e31ea1aaea1e5a
sayuprc <bepdielibe+github@gmail.com>
2025-04-20 21:23:56 +0900

    UseCase で JourneyLogLinkTypeFactoryInterface を利用するように修正

--- src/tests/Unit/JourneyLogLinkType/Application/Edit/EditInteractorTest.php
+++ src/tests/Unit/JourneyLogLinkType/Application/Edit/EditInteractorTest.php
@@ -6,17 +6,23 @@ namespace Tests\Unit\JourneyLogLinkType\Application\Edit;
 
 use JourneyLogLinkType\Application\Edit\EditInteractor;
 use JourneyLogLinkType\Domain\Models\JourneyLogLinkType;
+use JourneyLogLinkType\Domain\Models\JourneyLogLinkTypeFactoryInterface;
+use JourneyLogLinkType\Domain\Models\JourneyLogLinkTypeId;
+use JourneyLogLinkType\Domain\Models\JourneyLogLinkTypeName;
 use JourneyLogLinkType\Domain\Repositories\JourneyLogLinkTypeRepositoryInterface;
 use JourneyLogLinkType\UseCases\Edit\EditRequest;
 use JourneyLogLinkType\UseCases\Edit\EditUseCaseInterface;
 use Mockery;
 use Mockery\MockInterface;
 use PHPUnit\Framework\Attributes\Test;
+use Support\Domain\ValueObjects\OrderNo;
 use Tests\TestCase;
 
 class EditInteractorTest extends TestCase
 {
     private JourneyLogLinkTypeRepositoryInterface&MockInterface $journeyLogLinkTypeRepository;
+
+    private JourneyLogLinkTypeFactoryInterface&MockInterface $factory;
 
     private EditInteractor $interactor;
 
@@ -25,7 +31,8 @@ class EditInteractorTest extends TestCase
         parent::setUp();
 
         $this->journeyLogLinkTypeRepository = Mockery::mock(JourneyLogLinkTypeRepositoryInterface::class);
-        $this->interactor = new EditInteractor($this->journeyLogLinkTypeRepository);
+        $this->factory = Mockery::mock(JourneyLogLinkTypeFactoryInterface::class);
+        $this->interactor = new EditInteractor($this->journeyLogLinkTypeRepository, $this->factory);
     }
 
     #[Test]
@@ -37,10 +44,18 @@ class EditInteractorTest extends TestCase
     #[Test]
     public function editJourneyLogLinkType(): void
     {
+        $this->factory->shouldReceive('reconstitute')
+            ->with('AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA', 'リンク', 1)
+            ->andReturnUsing(fn (): JourneyLogLinkType => new JourneyLogLinkType(
+                new JourneyLogLinkTypeId('AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA'),
+                new JourneyLogLinkTypeName('リンク'),
+                new OrderNo(1),
+            ))
+            ->once();
+
         $this->journeyLogLinkTypeRepository->shouldReceive('update')
             ->with(Mockery::on(
-                fn ($arg) => $arg instanceof JourneyLogLinkType
-                    && $arg->journeyLogLinkTypeId->value === 'AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA'
+                fn (JourneyLogLinkType $arg): bool => $arg->journeyLogLinkTypeId->value === 'AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA'
                     && $arg->journeyLogLinkTypeName->value === 'リンク'
                     && $arg->orderNo->value === 1
             ))
