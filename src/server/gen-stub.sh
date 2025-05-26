9d7c15fc6faa39f07512f7cc24cff9cf3abf5a6d
sayuprc <bepdielibe+github@gmail.com>
2025-05-26 22:26:35 +0900

    生成コマンドを修正

--- src/server/gen-stub.sh
+++ src/server/gen-stub.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-protoc --proto_path=proto \
+protoc --proto_path=../proto \
   --php_out=. \
   --grpc_out=. \
   --plugin=protoc-gen-grpc=/usr/local/bin/grpc_php_plugin \
-  ./proto/shared/shared.proto \
-  ./proto/journey_log/journey_log.proto \
-  ./proto/journey_log_link_type/journey_log_link_type.proto \
-  ./proto/song/song.proto
+  ../proto/shared/shared.proto \
+  ../proto/journey_log/journey_log.proto \
+  ../proto/journey_log_link_type/journey_log_link_type.proto \
+  ../proto/song/song.proto
 
 composer dump-autoload
