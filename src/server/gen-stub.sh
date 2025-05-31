bb6a59c408e01f86a26f0097c16c94c981ff55f1
sayuprc <bepdielibe+github@gmail.com>
2025-05-31 14:55:36 +0900

    gRPC スタブ生成コードを修正

--- src/server/gen-stub.sh
+++ src/server/gen-stub.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-protoc --proto_path=../proto \
+protoc --proto_path=../proto/generated/protobuf \
   --php_out=. \
   --grpc_out=. \
   --plugin=protoc-gen-grpc=/usr/local/bin/grpc_php_plugin \
-  ../proto/shared/shared.proto \
-  ../proto/journey_log/journey_log.proto \
-  ../proto/journey_log_link_type/journey_log_link_type.proto \
-  ../proto/song/song.proto
+  ../proto/generated/protobuf/isekai_journey/shared.proto \
+  ../proto/generated/protobuf/isekai_journey/journey_log.proto \
+  ../proto/generated/protobuf/isekai_journey/journey_log_link_type.proto \
+  ../proto/generated/protobuf/isekai_journey/song.proto
 
 composer dump-autoload
