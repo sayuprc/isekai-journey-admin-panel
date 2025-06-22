8bc8b32020a5612af67a444d86793f66ffdb3887
sayuprc <bepdielibe+github@gmail.com>
2025-06-22 15:07:45 +0900

    既存コードを削除

--- src/server/tools/jane/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'openapi-file' => __DIR__ . '/../../../contracts/generated/oas/openapi.yaml',
-    'namespace' => 'Generated',
-    'directory' => __DIR__ . '/../../Generated',
-];
