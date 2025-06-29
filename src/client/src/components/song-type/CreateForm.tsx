0ef6747e30b64b4a679e2c1357e3e4f9737d35bb
sayuprc <bepdielibe+github@gmail.com>
2025-06-29 18:16:00 +0900

    クライアントのハンドリングを改善

--- src/client/src/components/song-type/CreateForm.tsx
+++ src/client/src/components/song-type/CreateForm.tsx
@@ -1,27 +1,37 @@
+import type { components } from '../../generated/schema';
 import { client } from '../../utils/client';
 import { setFlash } from '../Flash';
 
-export const Form = () => {
+export const CreateForm = () => {
   const handleSubmit = async (e: Event) => {
     e.preventDefault();
 
     const form = e.target as HTMLFormElement;
     const formData = new FormData(form);
 
-    const { data } = await client.POST('/song-types', {
+    const { data, error, response } = await client.POST('/song-types', {
       body: {
         songTypeName: formData.get('songTypeName')?.toString() ?? '',
         orderNo: Number(formData.get('orderNo')),
       },
     });
 
-    if (!data) {
+    // TODO リクエストはリポジトリ経由にし、レスポンス型を別途定義する
+    if (response.status === 400) {
+      // TODO わかりやすい表示にする
+      const errorAs = error as components['schemas']['ErrorResponse'];
+      alert(`リクエストが不正 ${errorAs.message}`);
+    } else if (response.status === 422) {
+      // TODO わかりやすい表示にする
+      const errorAs = error as components['schemas']['ValidationError'];
+      alert(`エラー ${errorAs.field}: ${errorAs.message}`);
+    } else if (!data) {
       // TODO エラーハンドリング
       throw new Error();
+    } else {
+      setFlash('作成しました');
+      window.location.href = '/song-types';
     }
-
-    setFlash('作成しました');
-    window.location.href = '/song-types';
   };
 
   return (
@@ -33,7 +43,7 @@ export const Form = () => {
         <input type="text" class="input" name="songTypeName" />
 
         <label class="label">表示順</label>
-        <input type="number" class="input" name="orderNo" required min="1" />
+        <input type="number" class="input" name="orderNo" required />
 
         <button class="btn btn-neutral mt-4">作成</button>
       </fieldset>
