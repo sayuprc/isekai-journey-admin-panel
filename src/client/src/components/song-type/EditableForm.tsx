9e7f0fdc9481ebd95a2d360ca557d1813a274b1a
sayuprc <bepdielibe+github@gmail.com>
2025-06-29 22:30:51 +0900

    クライアント側の処理を実装

--- src/client/src/components/song-type/EditableForm.tsx
+++ src/client/src/components/song-type/EditableForm.tsx
@@ -1,5 +1,6 @@
 import { onMount, Show } from 'solid-js';
 import type { components } from '../../generated/schema';
+import { client } from '../../utils/client';
 import { setFlash } from '../Flash';
 
 interface Props {
@@ -12,6 +13,47 @@ export const EditableForm = (props: Props) => {
     e.preventDefault();
 
     // TODO 実装する
+  };
+
+  const handleUpdate = async (e: Event) => {
+    e.preventDefault();
+
+    const form = (e.target as HTMLButtonElement).form as HTMLFormElement;
+    const formData = new FormData(form);
+
+    const songTypeId = props.data?.songType.songTypeId ?? '';
+
+    const { data, error, response } = await client.PUT('/song-types/{songTypeId}', {
+      params: {
+        path: {
+          songTypeId: songTypeId,
+        },
+      },
+      body: {
+        songTypeName: formData.get('songTypeName')?.toString() ?? '',
+        orderNo: Number(formData.get('orderNo')),
+      },
+    });
+
+    // TODO リクエストはリポジトリ経由にし、レスポンス型を別途定義する
+    if (response.status === 400) {
+      // TODO わかりやすい表示にする
+      const errorAs = error as components['schemas']['ErrorResponse'];
+      alert(`リクエストが不正 ${errorAs.message}`);
+    } else if (response.status === 404) {
+      setFlash('データがありません');
+      window.location.href = `/song-types`;
+    } else if (response.status === 422) {
+      // TODO わかりやすい表示にする
+      const errorAs = error as components['schemas']['ValidationError'];
+      alert(`エラー ${errorAs.field}: ${errorAs.message}`);
+    } else if (!data) {
+      // TODO エラーハンドリング
+      throw new Error();
+    } else {
+      setFlash('更新しました');
+      window.location.href = `/song-types`;
+    }
   };
 
   onMount(() => {
@@ -35,14 +77,14 @@ export const EditableForm = (props: Props) => {
           <legend class="fieldset-legend">楽曲種別詳細</legend>
 
           <label class="label">楽曲種別名</label>
-          <input type="text" class="input" value={props.data?.songType.songTypeName} />
+          <input type="text" class="input" name="songTypeName" value={props.data?.songType.songTypeName} />
 
           <label class="label">表示順</label>
-          <input type="number" class="input" required min="1" value={props.data?.songType.orderNo} />
+          <input type="number" class="input" name="orderNo" required min="1" value={props.data?.songType.orderNo} />
 
           <div class="flex justify-between gap-2">
             <button class="btn btn-error mt-4">削除</button>
-            <button class="btn btn-neutral mt-4">更新</button>
+            <button onClick={handleUpdate} class="btn btn-neutral mt-4">更新</button>
           </div>
         </fieldset>
       </form>
