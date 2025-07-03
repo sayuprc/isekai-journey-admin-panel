3326c93ed1778ba6be0e1aac7d80ea1c79c1d279
sayuprc <bepdielibe+github@gmail.com>
2025-07-03 23:57:51 +0900

    navbar を追加

---
+++ src/client/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+  return (
+    <div class="navbar bg-neutral-content text-base-content">
+      <div class="flex-1">ヰ世界のテラリウム</div>
+      <div class="flex-none">
+        <ul class="menu menu-horizontal px-1">
+          <li><a href="/song-types">楽曲種別一覧</a></li>
+          <li><a href="/creators">クリエイター一覧</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
