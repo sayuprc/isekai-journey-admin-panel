73c662527a6ad66c301a86e506054f5282f0bccd
sayuprc <bepdielibe+github@gmail.com>
2025-07-06 01:09:38 +0900

    ユーザーモデルを作成

---
+++ src/server/packages/User/Domain/Models/HashedPassword.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace User\Domain\Models;
+
+use Support\Domain\ValueObjects\String\StringValueObject;
+
+class HashedPassword extends StringValueObject
+{
+}
