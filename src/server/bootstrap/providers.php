9508bd5b30d6c3c463c91f2b82f36b4759388968
sayuprc <bepdielibe+github@gmail.com>
2025-07-06 23:27:10 +0900

    既存の認証パッケージを削除

--- src/server/bootstrap/providers.php
+++ src/server/bootstrap/providers.php
@@ -6,7 +6,6 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\Domain\SupportServiceProvider::class,
     App\Providers\Domain\UserServiceProvider::class,
-    App\Providers\Domain\AuthServiceProvider::class,
     App\Providers\Domain\JourneyLogServiceProvider::class,
     App\Providers\Domain\JourneyLogLinkTypeServiceProvider::class,
     App\Providers\Domain\SongServiceProvider::class,
