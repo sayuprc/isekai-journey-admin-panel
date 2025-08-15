9da3ad0b5c8149dbd19194ac2222f35e45d04c7a
sayuprc <bepdielibe+github@gmail.com>
2025-08-15 20:31:01 +0900

    Nullable な値を取得するメソッドを追加

--- src/server/packages/Support/Contracts/ConfigInterface.php
+++ src/server/packages/Support/Contracts/ConfigInterface.php
@@ -7,4 +7,10 @@ namespace Support\Contracts;
 interface ConfigInterface
 {
     public function getString(string $key): string;
+
+    public function getNullableString(string $key, ?string $default = null): ?string;
+
+    public function getNullableInt(string $key, ?int $default = null): ?int;
+
+    public function getNullableBool(string $key, ?bool $default = null): ?bool;
 }
--- src/server/packages/Support/Infrastructures/Config.php
+++ src/server/packages/Support/Infrastructures/Config.php
@@ -23,6 +23,48 @@ class Config implements ConfigInterface
         return $value;
     }
 
+    /**
+     * @throws Exception
+     */
+    public function getNullableString(string $key, ?string $default = null): ?string
+    {
+        $value = $this->get($key, $default);
+
+        if (! is_null($value) && ! is_string($value)) {
+            throw new Exception(sprintf('Config value for key "%s" is not a string or null', $key));
+        }
+
+        return $value;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getNullableInt(string $key, ?int $default = null): ?int
+    {
+        $value = $this->get($key, $default);
+
+        if (! is_null($value) && ! is_int($value)) {
+            throw new Exception(sprintf('Config value for key "%s" is not a int or null', $key));
+        }
+
+        return $value;
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function getNullableBool(string $key, ?bool $default = null): ?bool
+    {
+        $value = $this->get($key, $default);
+
+        if (! is_null($value) && ! is_bool($value)) {
+            throw new Exception(sprintf('Config value for key "%s" is not a bool or null', $key));
+        }
+
+        return $value;
+    }
+
     private function get(string $key, mixed $default = null): mixed
     {
         return config($key, $default);
