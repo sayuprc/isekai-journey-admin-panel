9bab9702e4d49deba6ffffce59a36e52442ef475
sayuprc <bepdielibe+github@gmail.com>
2025-12-03 01:21:06 +0900

    それぞれの参照を修正

--- src/server/tests/Feature/Api/SongType/ListSongTypeTest.php
+++ src/server/tests/Feature/Api/SongType/ListSongTypeTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Tests\Feature\Api\SongType;
 
 use PHPUnit\Framework\Attributes\Test;
+use SongType\Domain\Models\SongType;
 use SongType\Route\SongTypeRouteMap;
 use Tests\TestCase;
 
@@ -15,33 +16,13 @@ class ListSongTypeTest extends TestCase
     {
         $this->get(route(SongTypeRouteMap::List))
             ->assertStatus(200)
-            ->assertJson([
-                'songTypes' => [
-                    [
-                        'songTypeName' => 'オリジナル曲',
-                        'songTypeValue' => 1,
-                    ],
-                    [
-                        'songTypeName' => 'カバー曲',
-                        'songTypeValue' => 2,
-                    ],
-                    [
-                        'songTypeName' => 'コラボ曲',
-                        'songTypeValue' => 3,
-                    ],
-                    [
-                        'songTypeName' => '系譜曲',
-                        'songTypeValue' => 4,
-                    ],
-                    [
-                        'songTypeName' => '派生曲',
-                        'songTypeValue' => 5,
-                    ],
-                    [
-                        'songTypeName' => '拡声曲',
-                        'songTypeValue' => 6,
-                    ],
-                ],
+            ->assertExactJson([
+                'songTypes' => collect(SongType::cases())
+                    ->map(fn (SongType $songType): array => [
+                        'name' => $songType->getName(),
+                        'value' => $songType->value,
+                    ])
+                    ->all(),
             ]);
     }
 }
