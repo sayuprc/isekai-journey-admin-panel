e1f13fbfdb1ab6758d5b9989353a5fc6a61a577b
sayuprc <bepdielibe+github@gmail.com>
2026-01-22 23:26:41 +0900

    不要なコードを削除

--- src/server/tools/Arkitect/config.php
+++ src/server/tools/Arkitect/config.php
@@ -3,11 +3,8 @@
 declare(strict_types=1);
 
 use Tools\Arkitect\ComponentMaps\CreatorComponent;
-use Tools\Arkitect\ComponentMaps\JourneyLogComponent;
-use Tools\Arkitect\ComponentMaps\JourneyLogLinkTypeComponent;
 use Tools\Arkitect\ComponentMaps\LibraryComponent;
 use Tools\Arkitect\ComponentMaps\PerformerComponent;
-use Tools\Arkitect\ComponentMaps\SongComponent;
 use Tools\Arkitect\ComponentMaps\SongTypeComponent;
 use Tools\Arkitect\ComponentMaps\SupportComponent;
 use Tools\Arkitect\Define;
@@ -15,57 +12,6 @@ use Tools\Arkitect\Define;
 return [
     new Define(LibraryComponent::DateType),
     new Define(LibraryComponent::ResultType),
-
-    new Define(
-        JourneyLogComponent::Domain,
-        [
-            JourneyLogComponent::Domain,
-            JourneyLogLinkTypeComponent::Domain,
-            SupportComponent::Domain,
-            LibraryComponent::DateType,
-        ],
-    ),
-    new Define(
-        JourneyLogComponent::UseCase,
-        [
-            JourneyLogComponent::Domain,
-            LibraryComponent::DateType,
-            LibraryComponent::ResultType,
-        ],
-    ),
-
-    new Define(
-        JourneyLogLinkTypeComponent::Domain,
-        [
-            JourneyLogLinkTypeComponent::Domain,
-            SupportComponent::Domain,
-        ],
-    ),
-    new Define(
-        JourneyLogLinkTypeComponent::UseCase,
-        [
-            JourneyLogLinkTypeComponent::Domain,
-            LibraryComponent::ResultType,
-        ],
-    ),
-
-    new Define(
-        SongComponent::Domain,
-        [
-            SongComponent::Domain,
-            SongTypeComponent::Domain,
-            CreatorComponent::Domain,
-            SupportComponent::Domain,
-            LibraryComponent::DateType,
-        ],
-    ),
-    new Define(
-        SongComponent::UseCase,
-        [
-            SongComponent::Domain,
-            LibraryComponent::DateType,
-        ],
-    ),
 
     new Define(
         SongTypeComponent::Domain,
