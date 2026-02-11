b42e71f2b2516ba220bbde74e15dedb8f587c61e
sayuprc <bepdielibe+github@gmail.com>
2026-02-11 20:04:25 +0900

    クライアントを認証に対応させた

--- src/client/src/utils/client.ts
+++ src/client/src/utils/client.ts
@@ -1,4 +1,15 @@
-import createClient from 'openapi-fetch';
+import baseClient from 'openapi-fetch';
 import type { paths } from '../generated/schema';
 
-export const client = createClient<paths>({ baseUrl: import.meta.env.PUBLIC_API_URL });
+export const client = baseClient<paths>({
+  baseUrl: import.meta.env.PUBLIC_API_URL,
+});
+
+export const createClient = (request: Request) => {
+  return baseClient<paths>({
+    baseUrl: import.meta.env.PUBLIC_API_URL,
+    headers: {
+      cookie: request.headers.get('cookie') ?? '',
+    },
+  });
+};
