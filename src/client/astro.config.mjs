33231d4a9d3e3f5aa52155981a67c2be91d3c8c1
sayuprc <bepdielibe+github@gmail.com>
2026-02-23 12:56:23 +0900

    BFF を導入

--- src/client/astro.config.mjs
+++ src/client/astro.config.mjs
@@ -5,6 +5,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   server: {
     allowedHosts: ['local.admin.terrarium.isekaijoucho.fan'],
   },
