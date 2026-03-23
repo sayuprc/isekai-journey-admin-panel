459f27abdbf406dd7fbf28bc4533c22205cd080f
sayuprc <bepdielibe+github@gmail.com>
2026-03-24 00:27:29 +0900

    API エンドポイントを実装

---
+++ src/server/app/Http/Presenters/Api/SongTag/Converter.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Presenters\Api\SongTag;
+
+use OpenAPI\Client\Model\SongTag as OpenApiSongTag;
+use Song\Domain\Models\Tag\SongTag;
+
+class Converter
+{
+    public function toOpenApiSongTag(SongTag $tag): OpenApiSongTag
+    {
+        return new OpenApiSongTag()
+            ->setSongTagId($tag->songTagId->value)
+            ->setName($tag->name->value)
+            ->setOrderNo($tag->orderNo->value);
+    }
+}
--- src/server/app/Providers/Domain/SongServiceProvider.php
+++ src/server/app/Providers/Domain/SongServiceProvider.php
@@ -13,6 +13,7 @@ use Song\Application\Interactors\GetInteractor;
 use Song\Application\Interactors\ListAttributeInteractor;
 use Song\Application\Interactors\ListTypeInteractor;
 use Song\Application\Interactors\SearchInteractor;
+use Song\Application\Interactors\Tag\CreateInteractor as CreateTagInteractor;
 use Song\Application\Interactors\UpdateInteractor;
 use Song\Application\Query\SongQueryServiceInterface;
 use Song\Application\UseCase\Create\CreateInputData;
@@ -23,14 +24,20 @@ use Song\Application\UseCase\ListAttribute\ListAttributeUseCaseInterface;
 use Song\Application\UseCase\ListType\ListTypeUseCaseInterface;
 use Song\Application\UseCase\Search\SearchInputData;
 use Song\Application\UseCase\Search\SearchUseCaseInterface;
+use Song\Application\UseCase\Tag\Create\CreateInputData as CreateTagInputData;
+use Song\Application\UseCase\Tag\Create\CreateUseCaseInterface as CreateTagUseCaseInterface;
 use Song\Application\UseCase\Update\UpdateInputData;
 use Song\Application\UseCase\Update\UpdateUseCaseInterface;
 use Song\Domain\Models\SongFactoryInterface;
 use Song\Domain\Models\SongRepositoryInterface;
+use Song\Domain\Models\Tag\SongTagFactoryInterface;
+use Song\Domain\Models\Tag\SongTagRepositoryInterface;
 use Song\Infrastructures\CreatorUsageChecker;
 use Song\Infrastructures\SongFactory;
 use Song\Infrastructures\SongQueryService;
 use Song\Infrastructures\SongRepository;
+use Song\Infrastructures\SongTagFactory;
+use Song\Infrastructures\SongTagRepository;
 
 class SongServiceProvider extends EnvServiceProvider
 {
@@ -74,12 +81,27 @@ class SongServiceProvider extends EnvServiceProvider
 
         $this->registerSongAttribute();
 
+        $this->registerSongTag();
+
         $this->registerSongType();
     }
 
     private function registerSongAttribute(): void
     {
         $this->app->bind(ListAttributeUseCaseInterface::class, ListAttributeInteractor::class);
+    }
+
+    private function registerSongTag(): void
+    {
+        $this->app->bind(SongTagRepositoryInterface::class, SongTagRepository::class);
+        $this->app->bind(SongTagFactoryInterface::class, SongTagFactory::class);
+        $this->app->bind(CreateTagUseCaseInterface::class, CreateTagInteractor::class);
+
+        $this->app->bind(CreateTagInputData::class, function (): CreateTagInputData {
+            $request = $this->app->make(Request::class);
+
+            return $this->getMapper()->map(CreateTagInputData::class, $request->all());
+        });
     }
 
     private function registerSongType(): void
