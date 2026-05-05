49490e9933c25ac44b85dabec4f0a8063f717236
sayuprc <bepdielibe+github@gmail.com>
2026-05-06 01:36:26 +0900

    Reuse song value objects for media references

--- src/server/packages/Media/Domain/Models/MediaReferencedSong.php
+++ src/server/packages/Media/Domain/Models/MediaReferencedSong.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace Media\Domain\Models;
 
+use Song\Domain\Models\SongId;
+use Song\Domain\Models\Title;
 use Support\Domain\ValueObjects\OrderNo;
 
 readonly class MediaReferencedSong
 {
     public function __construct(
         public SongId $songId,
-        public SongTitle $title,
+        public Title $title,
         public OrderNo $songOrderNo,
         public OrderNo $mediaOrderNo,
     ) {
@@ -24,7 +26,7 @@ readonly class MediaReferencedSong
     ): self {
         return new self(
             SongId::reconstruct($songId),
-            SongTitle::reconstruct($title),
+            Title::reconstruct($title),
             OrderNo::reconstruct($songOrderNo),
             OrderNo::reconstruct($mediaOrderNo),
         );
