95ad653b399db25028112ca66942ed4207dfb6c4
sayuprc <bepdielibe+github@gmail.com>
2026-05-08 14:34:24 +0900

    Release repository の器だけを残す

--- src/server/packages/Release/Domain/Models/ReleaseRepositoryInterface.php
+++ src/server/packages/Release/Domain/Models/ReleaseRepositoryInterface.php
@@ -6,9 +6,4 @@ namespace Release\Domain\Models;
 
 interface ReleaseRepositoryInterface
 {
-    public function find(ReleaseId $releaseId): ?Release;
-
-    public function save(Release $release): Release;
-
-    public function delete(ReleaseId $releaseId): void;
 }
