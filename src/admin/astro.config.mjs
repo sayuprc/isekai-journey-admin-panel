368ed42f058f72a9f45e0d766975105026211e70
sayuprc <bepdielibe+github@gmail.com>
2026-06-14 11:33:18 +0900

    admin を改善

--- src/admin/astro.config.mjs
+++ src/admin/astro.config.mjs
@@ -1,7 +1,8 @@
 // @ts-check
+import node from '@astrojs/node';
 import solidJs from '@astrojs/solid-js';
 import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 const port = Number(import.meta.env.PORT ?? '4321');
 
@@ -13,11 +14,25 @@ export default defineConfig({
     port: port,
     allowedHosts: ['local.admin.isekaijoucho.fan'],
   },
+  adapter: node({
+    mode: 'standalone',
+  }),
   security: {
     checkOrigin: false,
   },
+  env: {
+    schema: {
+      API_URL: envField.string({ context: 'server', access: 'secret' }),
+      CACHE_URL: envField.string({ context: 'server', access: 'secret' }),
+      CACHE_TOKEN: envField.string({ context: 'server', access: 'secret' }),
+      PUBLIC_APP_URL: envField.string({ context: 'client', access: 'public' }),
+    },
+  },
   vite: {
     plugins: [tailwindcss()],
+    ssr: {
+      noExternal: true,
+    },
   },
   integrations: [solidJs()],
 });
