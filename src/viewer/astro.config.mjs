47ac9eb253a470c9d04326e8742fbf46ca1acaad
sayuprc <41261915+sayuprc@users.noreply.github.com>
2026-07-25 23:05:18 +0900

    メタデータを編集 (#885)

--- src/viewer/astro.config.mjs
+++ src/viewer/astro.config.mjs
@@ -1,15 +1,19 @@
 // @ts-check
+import process from 'node:process';
+import sitemap from '@astrojs/sitemap';
 import solidJs from '@astrojs/solid-js';
 import { defineConfig } from 'astro/config';
 
 const port = Number(import.meta.env.PORT ?? '3000');
+const site = process.env.SITE_URL ?? 'https://local.isekaijoucho.fan';
 
 // https://astro.build/config
 export default defineConfig({
+  site: site,
   server: {
     host: true,
     port: port,
     allowedHosts: ['local.isekaijoucho.fan'],
   },
-  integrations: [solidJs()],
+  integrations: [solidJs(), sitemap()],
 });
